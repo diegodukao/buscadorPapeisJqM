651ac93000fb014f519026256f9c12e3183d285e
diegodukao <diegodukao@gmail.com>
2013-05-06 11:09:33 -0300

    showing a map (without user location)

--- PhoneGap/assets/www/js/googlemaps.js
+++ PhoneGap/assets/www/js/googlemaps.js
@@ -1,27 +1,67 @@
-function callMap() {
+//function GoogleMap() {
+    //this.initialize = function(){
+        //var map = showMap();
+        //console.log("############# INITIALIZE MAP");
+    //}
+     
+    //var showMap = function(){
+        //var mapOptions = {
+            //zoom: 4,
+            //center: new google.maps.LatLng(-33, 151),
+            //mapTypeId: google.maps.MapTypeId.ROADMAP
+        //}
+         
+        //var map = new google.maps.Map(document.getElementById("mapa_canvas"), mapOptions);
+         
+        //return map;
+    //}
+//}
+
+//function callMap() {
     //console.log("#######CHAMARA O MAPA!");
     
     //var map = new GoogleMap();
-    //console.log(map);
     //map.initialize();
     
-    navigator.geolocation.getCurrentPosition(onGeoSuccess, onError, {enableHighAccuracy:true});
+    ////navigator.geolocation.getCurrentPosition(onGeoSuccess, onError, {enableHighAccuracy:true});
+//}
+
+var map1, latlng1, options1;
+function initialize() {
+
+    latlng1 = new google.maps.LatLng(40.716948, -74.003563);
+    options1 = { zoom: 14, center: latlng1, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    map1 = new google.maps.Map(document.getElementById("mapa_canvas"), options1);
+
 }
+$('#mapa').live("pagecreate", function() {
+
+    initialize();
+
+});
+
+$('#mapa').live('pageshow',function(){
+
+    //console.log("test");
+    google.maps.event.trigger(map1, 'resize');
+    map1.setOptions(options1); 
+
+});
 
 // onSuccess Callback
 //   This method accepts a `Position` object, which contains
 //   the current GPS coordinates
 //
-var onGeoSuccess = function(position) {
-    alert('Latitude: '          + position.coords.latitude          + '\n' +
-          'Longitude: '         + position.coords.longitude         + '\n' +
-          'Altitude: '          + position.coords.altitude          + '\n' +
-          'Accuracy: '          + position.coords.accuracy          + '\n' +
-          'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          'Heading: '           + position.coords.heading           + '\n' +
-          'Speed: '             + position.coords.speed             + '\n' +
-          'Timestamp: '         + position.timestamp                + '\n');
-};
+//var onGeoSuccess = function(position) {
+    //alert('Latitude: '          + position.coords.latitude          + '\n' +
+          //'Longitude: '         + position.coords.longitude         + '\n' +
+          //'Altitude: '          + position.coords.altitude          + '\n' +
+          //'Accuracy: '          + position.coords.accuracy          + '\n' +
+          //'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+          //'Heading: '           + position.coords.heading           + '\n' +
+          //'Speed: '             + position.coords.speed             + '\n' +
+          //'Timestamp: '         + position.timestamp                + '\n');
+//};
 
 //function GoogleMap() {
     //this.initialize = function() {
