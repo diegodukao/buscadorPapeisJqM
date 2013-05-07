1e407af7a2f75b52fa478932d5fc203f116f1bab
diegodukao <diegodukao@gmail.com>
2013-05-07 09:41:34 -0300

    showing current location and updating map center if location is changed

--- PhoneGap/assets/www/js/googlemaps.js
+++ PhoneGap/assets/www/js/googlemaps.js
@@ -1,98 +1,28 @@
-//function GoogleMap() {
-    //this.initialize = function(){
-        //var map = showMap();
-        //console.log("############# INITIALIZE MAP");
-    //}
-     
-    //var showMap = function(){
-        //var mapOptions = {
-            //zoom: 4,
-            //center: new google.maps.LatLng(-33, 151),
-            //mapTypeId: google.maps.MapTypeId.ROADMAP
-        //}
-         
-        //var map = new google.maps.Map(document.getElementById("mapa_canvas"), mapOptions);
-         
-        //return map;
-    //}
-//}
+var map, latlng, options, watchId;
 
-//function callMap() {
-    //console.log("#######CHAMARA O MAPA!");
-    
-    //var map = new GoogleMap();
-    //map.initialize();
-    
-    ////navigator.geolocation.getCurrentPosition(onGeoSuccess, onError, {enableHighAccuracy:true});
-//}
-
-var map1, latlng1, options1;
+function initialize() {
+    latlng = new google.maps.LatLng('-33.890542','151.274856');
+    options = { zoom: 14, center: latlng, mapTypeId: google.maps.MapTypeId.ROADMAP };
+    map = new google.maps.Map(document.getElementById("mapa_canvas"), options);
+}
 
 var onGeoSuccess = function(position) {
-    latlng1 = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    options1 = { zoom: 14, center: latlng1, mapTypeId: google.maps.MapTypeId.ROADMAP };
-    map1 = new google.maps.Map(document.getElementById("mapa_canvas"), options1);
+    var currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    map.setCenter(currentLocation);
+    var marker = new google.maps.Marker({
+        position: currentLocation,
+        map: map,
+        title:"You are here!"
+    });
 };
 
 $('#mapa').live("pagecreate", function() {
-    navigator.geolocation.getCurrentPosition(onGeoSuccess, onError, {enableHighAccuracy:true});
+    initialize();
+    watchId = navigator.geolocation.watchPosition(onGeoSuccess, onError, {enableHighAccuracy:true});
 });
 
-$('#mapa').live('pageshow',function(){
-    google.maps.event.trigger(map1, 'resize');
-    map1.setOptions(options1); 
-});
-
-// onSuccess Callback
-//   This method accepts a `Position` object, which contains
-//   the current GPS coordinates
-//
-//var onGeoSuccess = function(position) {
-    //alert('Latitude: '          + position.coords.latitude          + '\n' +
-          //'Longitude: '         + position.coords.longitude         + '\n' +
-          //'Altitude: '          + position.coords.altitude          + '\n' +
-          //'Accuracy: '          + position.coords.accuracy          + '\n' +
-          //'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-          //'Heading: '           + position.coords.heading           + '\n' +
-          //'Speed: '             + position.coords.speed             + '\n' +
-          //'Timestamp: '         + position.timestamp                + '\n');
-//};
-
-//function GoogleMap() {
-    //this.initialize = function() {
-        //console.log("#######MAPA INITIALIZE!");
-        //var map = showMap();
-        //addMarkersToMap(map);
-    //}
-     
-    //var showMap = function() {
-        //var mapOptions = {
-            //zoom: 4,
-            //center: new google.maps.LatLng(-33, 151),
-            //mapTypeId: google.maps.MapTypeId.ROADMAP
-        //}
-        
-        //console.log(document.getElementById("map_canvas"));
-        //var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-        
-        //console.log("#######show map!!!");
-        //return map;
-    //}
-    
-    //var addMarkersToMap = function(map) {
-        //var latitudeAndLongitudeOne = new google.maps.LatLng('-33.890542','151.274856');
-         
-        //var markerOne = new google.maps.Marker({
-            //position: latitudeAndLongitudeOne,
-            //map: map
-        //});
-         
-        //var latitudeAndLongitudeTwo = new google.maps.LatLng('57.77828', '14.17200');
-         
-        //var markerOne = new google.maps.Marker({
-            //position: latitudeAndLongitudeTwo,
-            //map: map
-        //});
-    //}
-//};
+//$('#mapa').live('pageshow',function(){
+    //google.maps.event.trigger(map, 'resize');
+    //map.setOptions(options); 
+//});
 
